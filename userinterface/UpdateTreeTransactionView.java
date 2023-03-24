041d6402e7937ed1292ba4534d601ca62d805ef8
asprague <aaron0sprague@gmail.com>
2023-03-24 18:03:37 -0400

    View change

--- userinterface/UpdateTreeTransactionView.java
+++ userinterface/UpdateTreeTransactionView.java
@@ -1,4 +1,162 @@
 package userinterface;
+import javafx.event.Event;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
-public class UpdateTreeTransactionView {
+import java.util.Properties;
+import java.util.Vector;
+
+// project imports
+import impresario.IModel;
+import model.*;
+//==============================================================
+public class UpdateTreeTransactionView extends View {
+    private TextField barcode;
+    private Button submitButton;
+    private Button cancelButton;
+    private MessageView statusLog;
+    //-------------------------------------------------------------
+    public UpdateTreeTransactionView(IModel trans) {
+        super(trans, "UpdateTreeTransactionView");
+        // create a container for showing the contents
+        VBox container = new VBox(10);
+        container.setPadding(new Insets(15, 5, 5, 5));
+        // create our GUI components, add them to this panel
+        container.getChildren().add(createTitle());
+        container.getChildren().add(createFormContent());
+        // Error message area
+        container.getChildren().add(createStatusLog("                          "));
+        getChildren().add(container);
+        populateFields();
+    }
+    //-------------------------------------------------------------
+    // Creates Title for Container
+    private Node createTitle() {
+        Text titleText = new Text("       Troop 209          ");
+        titleText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        titleText.setTextAlignment(TextAlignment.CENTER);
+        titleText.setFill(Color.DARKGREEN);
+        return titleText;
+    }
+    // Create the main data entry fields
+    //-------------------------------------------------------------
+    private VBox createFormContent()
+    {
+        VBox vbox = new VBox(10);
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+//-------------------------------------------------------------------
+        Label barcodeLabel = new Label("Barcode : ");
+        grid.add(barcodeLabel, 0, 0);
+
+        barcode = new TextField();
+        barcode.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                processAction(e);
+            }
+        });
+        grid.add(barcode, 1, 0);
+//------------------------------------------------------------------
+        submitButton = new Button("Submit");
+        submitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                clearErrorMessage();
+                processAction(e);
+            }
+        });
+//----------------------------------------------------------------
+        cancelButton = new Button("Back");
+        cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                /**
+                 * Process the Cancel button.
+                 * The ultimate result of this action is that the transaction will tell the teller to
+                 * to switch to the transaction choice view. BUT THAT IS NOT THIS VIEW'S CONCERN.
+                 * It simply tells its model (controller) that the deposit transaction was canceled, and leaves it
+                 * to the model to decide to tell the teller to do the switch back.
+                 */
+                //----------------------------------------------------------
+                clearErrorMessage();
+                myModel.stateChangeRequest("CancelDeposit", null);
+            }
+        });
+        HBox btnContainer = new HBox(100);
+        btnContainer.setAlignment(Pos.CENTER);
+        btnContainer.getChildren().add(submitButton);
+        btnContainer.getChildren().add(cancelButton);
+        vbox.getChildren().add(grid);
+        vbox.getChildren().add(btnContainer);
+        return vbox;
+    }
+    //-------------------------------------------------------------
+    private MessageView createStatusLog(String initialMessage) {
+        statusLog = new MessageView(initialMessage);
+        return statusLog;
+    }
+    //-------------------------------------------------------------
+    public void populateFields() {
+    }
+    //----------------------------------------------------------
+    private void processAction(Event evt) {
+        //clearErrorMessage();
+        String barcodeEntered = barcode.getText();
+        if ((barcodeEntered == null) || (barcodeEntered.length() == 0))
+        {
+            displayErrorMessage("Please enter a barcode");
+        } else {
+            processTreeInfo(barcodeEntered);
+        }
+    }
+    private void processTreeInfo(String barcode) {
+        Properties props = new Properties();
+        props.setProperty("barcode", barcode);
+        myModel.stateChangeRequest("RegisterTreeInfo", props);
+        Tree tree = new Tree(props);
+        tree.update();
+        displayMessage("Successfully added new Tree");
+    }
+    public void displayMessage(String message)
+    {
+        statusLog.displayMessage(message);
+    }
+
+    //---------------------------------------------------------
+    public void updateState(String key, Object value) {
+    }
+    //----------------------------------------------------------
+    public void displayErrorMessage(String message)
+    {
+        statusLog.displayErrorMessage(message);
+    }
+    //----------------------------------------------------------
+    public void clearErrorMessage()
+    {
+        statusLog.clearErrorMessage();
+    }
 }
